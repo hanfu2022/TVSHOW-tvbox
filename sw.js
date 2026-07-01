51ad925fd2b3ea249577d07105309b5e6e91a2d4
hanfu2022 <83848191+hanfu2022@users.noreply.github.com>
2026-07-01 22:20:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11224324
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
-
-self.options = {
     "domain": "3nbf4.com",
     "zoneId": 11225035
 }
